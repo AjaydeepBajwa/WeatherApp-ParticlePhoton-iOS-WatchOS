6d54eb4d2be6cc9a73a1f3f1ccd9dd813558f646
AJAY-MAC <letv4545@gmail.com>
2019-11-04 18:36:09 -0500

    Connecting to Weather Api and getting/printing temperature, humidity, pressure etc to console using Alamofire,SwiftyJSON

--- ParticleWatchiOS WatchKit Extension/InterfaceController.swift
+++ ParticleWatchiOS WatchKit Extension/InterfaceController.swift
@@ -8,6 +8,8 @@
 
 import WatchKit
 import Foundation
+import Alamofire
+import SwiftyJSON
 
 
 class InterfaceController: WKInterfaceController {
@@ -21,6 +23,31 @@ class InterfaceController: WKInterfaceController {
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        
+        //let URL = "https://api.sunrise-sunset.org/json?lat=\(lat!)&lng=\(lng!)&date=today"
+        let URL = "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=8eb59ef603c67740b0e5b7b8725d2ff3"
+        
+        Alamofire.request(URL).responseJSON{
+            response in
+            guard let apiData = response.result.value else{
+                print("Error getting response from url")
+                return
+            }
+            print(apiData)
+            
+            let jsonResponse = JSON(apiData)
+            let weatherDescription = jsonResponse["weather"].array![0]["description"].string
+            let temperature = jsonResponse["main"]["temp"].float
+            let pressure = jsonResponse["main"]["pressure"].float
+            let humidity = jsonResponse["main"]["humidity"].float
+            let country = jsonResponse["sys"]["country"].string
+            
+            print("Weather description : \(weatherDescription!)")
+            print("Temperature : \(temperature!)")
+            print("Pressure: \(pressure!)")
+            print("Humidity: \(humidity!)")
+            print("Country: \(country!)")
+        }
     }
     
     override func didDeactivate() {
